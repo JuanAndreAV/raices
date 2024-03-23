60d9d0e0ca921051ab6bbb6dc7bdf0cad9da7f29
Juan Andres Alvarez <juanandres1029@gmail.com>
2024-03-23 07:31:13 -0500

    routes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+
+
+const routes: Routes = [
+    { path: 'home',
+      component: PortafolioComponent
+    },
+    { path: 'about',
+      component: AboutComponent
+    },
+    { path: 'item',
+      component: ItemComponent
+    },
+    { path: '**',
+      component: PortafolioComponent
+    },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {useHash:true})],//para desplegar en github pages useHash
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
